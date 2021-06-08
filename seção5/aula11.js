e11fe024d250c1056b4559a54562afa0da2b489b
Tonolli <gtcislaghi@gmail.com>
2021-06-08 17:22:56 -0300

    aula 11 try catch

---
+++ seção5/aula11.js
@@ -0,0 +1,33 @@
+//console.log(nome);//ReferenceError
+
+//console.log('Oi...');
+
+//console.log(4.Oi());
+/*
+try{
+    console.log(nome);
+}catch(e){
+    console.log('Não foi possível imprimir nome.');
+    console.log(e.name);
+    console.log(e.message);
+}
+
+console.log("Oi...")
+*/
+//Lançando erros
+function dividir(num1, num2){
+    if(num1 === 0 || num2 === 0){
+        throw("Os valores devem ser positivos."); //lançando uma exceção
+    }else{
+        return num1 / num2;
+    }
+}
+
+try{
+let ret = dividir(8, 0);
+console.log(ret);
+}catch(e){
+    console.log('Não foi possível dividir.');
+}finally{
+    console.log('Vamos continuar...');
+}
