c3915d529dce502630d6f92dbf0a54fa9ebe6913
Tonolli <gtcislaghi@gmail.com>
2021-06-08 16:52:35 -0300

    aula 8 template strings

---
+++ seção5/aula8.js
@@ -0,0 +1,14 @@
+const idade = 19;
+const nome = 'Geek';
+
+function soma_3(idade){
+    return idade + 3;
+}
+
+console.log(nome + ' tem ' + idade + ' anos. ');
+
+// Template strings
+
+console.log(`${nome} tem ${soma_3(idade)} anos.`);
+
+// Em template string usa-se cráse, e não aspas simples ou acento agudo
