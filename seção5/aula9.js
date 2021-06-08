d8bbf2bb019640463d7e6e59401ccf7985d7af4e
Tonolli <gtcislaghi@gmail.com>
2021-06-08 16:58:48 -0300

    aula 9 hosting

---
+++ seção5/aula9.js
@@ -0,0 +1,13 @@
+/*console.log(nome); //undefined - aqui ocorre o Hoisting içamento
+
+var noem = 'Geek'; //declarando e inicializando a variável
+
+//console.log(nome);
+
+*/
+
+console.log(idade + 4); // NaN - not a Number
+
+var idade = 23;
+
+console.log(idade);
